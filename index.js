595f70d5a91aa44d7e4a04322c3960af61c9c39b
etothepowerof26 <jdhale2004@gmail.com>
2020-09-30 21:43:02 -0400

    Commit test code

---
+++ index.js
@@ -0,0 +1,30 @@
+window.onload = (event) =>
+{
+	let box = document.getElementsByClassName("glres")[0];
+	box.innerHTML = "";
+		
+	let search_elem = document.getElementsByClassName("search")[0];
+		
+	let current_search_query = "";
+	search_elem.addEventListener('input', (ev) =>
+	{
+		var value = search_elem.value;
+		box.innerHTML = "";
+		
+		if( value != "" )
+		{
+			let objects = [];
+			
+			for( item in ITEMS )
+			{
+				var itm = ITEMS[item];
+				var start = itm.name.toLowerCase();
+				if( start.search( value.toLowerCase() ) != -1 )
+				{
+					// todo: sort by name
+					// objects.push(itm);
+				}
+			}
+		}
+	});
+};
---
+++ items.js
@@ -0,0 +1,61 @@
+let ITEMS = {
+	"0": {
+		"name": "Finality",
+		"icon": "icons/finality.png",
+		"slot": 2,
+		"obtained": [
+			"Completing Expert challenge scrolls",
+			"Completing Professional challenge scrolls"
+		],
+		"stats": {
+			"clip": "+2",
+			"acc": "+18%",
+			"deploy": "-50%",
+			"frate": "+17%",
+			"stab": "+18%",
+			"dmg": "+22%",
+			"rspeed": "+15%",
+			"mobi": "-5%",
+		},
+		"drop": [
+			"All suffixes (challenge + natural)",
+			"Add ammo"
+		],
+		"features": {
+			"Thermal Scope": "See people even if they're cloaked. FPS killer."
+		},
+		"abilities": {
+			"Hold E": "Spawns orb that follows you to damage anyone nearby and anyone who you shoot."
+		}
+	},
+	"1": {
+		"name": "brah",
+		"icon": "icons/finality.png",
+		"slot": 2,
+		"obtained": [
+			"Completing Expert challenge scrolls",
+			"Completing Professional challenge scrolls"
+		],
+		"stats": {
+			"clip": "+2",
+			"acc": "+18%",
+			"deploy": "-50%",
+			"frate": "+17%",
+			"stab": "+18%",
+			"dmg": "+22%",
+			"rspeed": "+15%",
+			"mobi": "-5%",
+		},
+		"drop": [
+			"All suffixes (challenge + natural)",
+			"Add ammo"
+		],
+		"features": {
+			"Thermal Scope": "See people even if they're cloaked. FPS killer."
+		},
+		"abilities": {
+			"Hold E": "Spawns orb that follows you to damage anyone nearby and anyone who you shoot."
+		}
+	},
+	
+}
