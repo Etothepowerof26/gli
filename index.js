25d3470ee56db116fdc904d11fcc2c2bbdd99c4e
etothepowerof26 <jdhale2004@gmail.com>
2020-10-01 11:26:45 -0400

    Add more items, add Celestial and Exotic background

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
+// util
+GetFirstFromClassName = (cl) => document.getElementsByClassName(cl)[0];
+function RunSearchWithQuery( search )
+{
+	let ret = [];
+	
+	for( item in ITEMS )
+	{
+		let n = ITEMS[ item ].name;
+		if( n )
+		{
+			let name = n.toLowerCase();
+			if( n.search( search.toLowerCase() ) != -1 && ret.length < 10 )
+			{
+				ret.push( ITEMS[ item ] );
+			}
+		}
+	}
+	
+	return ret;
+}
+
+function HandleOnHover( main, item )
+{
+	return (event) =>
+	{
+		main.src = item.icon;
+		// console.log(item.icon, main);
+	};
+}
+
+function HandleOnClickForItem( main, item )
+{
+	return (event) =>
+	{
+		alert('Godlike pressed');
+		console.log(item);
+		main.style.display = "none";
+	};
+}
+
+
+
 window.onload = (event) =>
 {
-	let box = document.getElementsByClassName("glres")[0];
+	
+	let main = GetFirstFromClassName('main');
+	let preview = document.getElementById('preview');
+	
+	let box = GetFirstFromClassName("glres");
 	box.innerHTML = "";
 		
-	let search_elem = document.getElementsByClassName("search")[0];
+	let search_elem = GetFirstFromClassName("search");
 		
 	let current_search_query = "";
 	search_elem.addEventListener('input', (ev) =>
 	{
-		var value = search_elem.value;
+		let value = search_elem.value;
 		box.innerHTML = "";
 		
 		if( value != "" )
 		{
-			let objects = [];
-			
-			for( item in ITEMS )
+			let objects = RunSearchWithQuery( value );
+			for( let i = 0; i < objects.length; i++ )
 			{
-				var itm = ITEMS[item];
-				var start = itm.name.toLowerCase();
-				if( start.search( value.toLowerCase() ) != -1 )
-				{
-					// todo: sort by name
-					console.log( itm );
-				}
+				let item = objects[i];
+				
+				let div = document.createElement('div');
+				if( item.rarity === "Celestial" )
+					div.className = "celestialbg";
+				else if( item.rarity === "Exotic" )
+					div.className = "exoticbg";
+				else
+					div.className = "glreslabel";
+				
+				div.innerHTML = item.name;
+				div.onclick = HandleOnClickForItem( main, item );
+				div.onmouseover = HandleOnHover( preview, item );
+				
+				box.appendChild(div);
 			}
 		}
 	});
--- items.js
+++ items.js
@@ -1,61 +1,121 @@
-let ITEMS = {
-	"0": {
-		"name": "Finality",
-		"icon": "icons/finality.png",
-		"slot": 2,
-		"obtained": [
-			"Completing Expert challenge scrolls",
-			"Completing Professional challenge scrolls"
-		],
-		"stats": {
-			"clip": "+2",
-			"acc": "+18%",
-			"deploy": "-50%",
-			"frate": "+17%",
-			"stab": "+18%",
-			"dmg": "+22%",
-			"rspeed": "+15%",
-			"mobi": "-5%",
+let ITEMS = [
+	{
+		"name": "Aezurth", "icon": "icons/aezurth.png", "slot": 3,
+		"obtained": [ "Completing the Castle Event" ],
+		"stats": {},
+		"drop": [ "Torment", "Double Shoot", "Suffixes(?)" ],
+		"features": { "Infinite Ammo": "At max level, you have infinite ammo. Works like excelerated tier at max level." },
+		"abilities": { "Hold RMB (Max Level)": "Shoots a black hole." },
+		"tags": [ "Enhancable", "Event Drop" ]
+	}
+]
+
+// Godswords
+{
+	let HILTS = [ "Anarchic", "Barbaric", "Avian", "Sagacious" ]
+	for( h in HILTS )
+	{
+		let hilt = HILTS[h];
+		let sword = {
+			"name": `${hilt} Godsword`, "icon": `icons/${hilt.toLowerCase()}.png`, "slot": 1,
+			"obtained": [ `Forging Godsword shards 1,2,3 + ${hilt} hilt at Arvid` ],
+			"stats": {},
+			"drop": [ "Suffixes(?)" ],
+			"features": { },
+			"abilities": { "RMB": "TODO" },
+			"tags": [ "Forged", "Godsword" ]
+		};
+		console.log(hilt);
+		ITEMS.push( sword );
+	}
+	ITEMS.push( {
+		"name": `Elder Godsword`, "icon": "icons/elder.png", "slot": 1,
+		"obtained": [ `Forging Godsword shards 1,2,3 + All sword hilts + Ancient Gemstone at Arvid` ],
+		"stats": {},
+		"drop": [ "Suffixes(?)" ],
+		"features": { },
+		"abilities": { "RMB": "TODO" },
+		"tags": [ "Forged", "Godsword" ],
+		"rarity": "Celestial"
+	} );
+}
+
+// Books
+{
+	let PAGES = [ "I. Chapter of Arushiv", "II. Chapter of Igneous", "III. Chapter of Regret" ];
+	let DROP = {
+		"I. Chapter of Arushiv": "the survive bosses that appear every 12 rounds.",
+		"II. Chapter of Igneous": "the chest that appears from killing Morgoth.",
+		"III. Chapter of Regret": "killing Bloodwynd."
+	};
+	// todo
+	let ABILITY = {
+		"I. Chapter of Arushiv": "the survive bosses that appear every 12 rounds.",
+		"II. Chapter of Igneous": "the chest that appears from killing Morgoth.",
+		"III. Chapter of Regret": "killing Bloodwynd."
+	};
+	let IMAGE = {
+		"I. Chapter of Arushiv": "arushiv",
+		"II. Chapter of Igneous": "igneous",
+		"III. Chapter of Regret": "regret"
+	};
+	for( h in PAGES )
+	{
+		let hilt = PAGES[h];
+		let sword = {
+			"name": `${hilt}`, "icon": `icons/${IMAGE[hilt]}.png`, "slot": 1,
+			"obtained": [ `Dropping it from ${DROP[hilt]}` ],
+			"stats": {},
+			"drop": [ "N/A" ],
+			"features": { },
+			"abilities": { "RMB": "TODO" },
+			"tags": [ "Page", "Grimoire" ]
+		};
+		console.log(hilt);
+		ITEMS.push( sword );
+	}
+	ITEMS.push( {
+		"name": `Creed of Necros`, "icon": "icons/finality.png", "slot": 1,
+		"obtained": [ `Obtaining all grimoires + a blank one and talking to Arvid` ],
+		"stats": {},
+		"drop": [ "N/A" ],
+		"features": { },
+		"abilities": { "RMB": "TODO" },
+		"tags": [ "Grimoire" ],
+		"rarity": "Celestial"
+	} );
+}
+
+// Torments
+{
+	let TORMENTS = {
+		"Desolation": {
+			"img": "desolation",
+			"scroll": "Hard"
 		},
-		"drop": [
-			"All suffixes (challenge + natural)",
-			"Add ammo"
-		],
-		"features": {
-			"Thermal Scope": "See people even if they're cloaked. FPS killer."
+		"Demise": {
+			"img": "demise",
+			"scroll": "Expert"
+			
 		},
-		"abilities": {
-			"Hold E": "Spawns orb that follows you to damage anyone nearby and anyone who you shoot."
+		"Oblivion": {
+			"img": "oblivion",
+			"scroll": "Professional"
 		}
-	},
-	"1": {
-		"name": "brah",
-		"icon": "icons/finality.png",
-		"slot": 2,
-		"obtained": [
-			"Completing Expert challenge scrolls",
-			"Completing Professional challenge scrolls"
-		],
-		"stats": {
-			"clip": "+2",
-			"acc": "+18%",
-			"deploy": "-50%",
-			"frate": "+17%",
-			"stab": "+18%",
-			"dmg": "+22%",
-			"rspeed": "+15%",
-			"mobi": "-5%",
-		},
-		"drop": [
-			"All suffixes (challenge + natural)",
-			"Add ammo"
-		],
-		"features": {
-			"Thermal Scope": "See people even if they're cloaked. FPS killer."
-		},
-		"abilities": {
-			"Hold E": "Spawns orb that follows you to damage anyone nearby and anyone who you shoot."
-		}
-	},
-	
+	}
+	for( t in TORMENTS )
+	{
+		let tr = TORMENTS[t];
+		ITEMS.push( {
+			"name": `Tormented ${t}`, "icon": `icons/${tr.img}.png`, "slot": 1,
+			"obtained": [ `Completing a ${tr.scroll} Challenge Scroll` ],
+			"stats": "Varies between weapons",
+			"drop": [ "Scroll Suffixes" ],
+			"features": { "Ability Charge": "Charge up your RMB ability by shooting enemies." },
+			"abilities": { "RMB": "Gain infinite ammo for 10 seconds. Firerate and damage increased slightly." },
+			"tags": [ "Scroll", "Unique", "Socketable" ]
+		} );
+		
+		console.log(t);
+	}
 }
